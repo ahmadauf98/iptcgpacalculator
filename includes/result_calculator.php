460fcd1bc99d75b307676d7c1d88a1722e2d7d75
Ahmad Auf Bin Nasruddin <32447372+ahmadauf98@users.noreply.github.com>
2019-07-25 01:43:24 +0800

    Update result_calculator.php

--- includes/result_calculator.php
+++ includes/result_calculator.php
@@ -2,6 +2,11 @@
     if (empty($totalCredit)|| empty($totalGradePoints)) {
     echo "";
     }
+
+    elseif((empty($grade1) && empty($ch1)) || (empty($grade2) && empty($ch2)) || (empty($grade3) && empty($ch3)) || (empty($grade4) && empty($ch4)) || (empty($grade5) && empty($ch5)) || (empty($grade6) && empty($ch6)) || (empty($grade7) && empty($ch7)) || (empty($grade8) && empty($ch8))){
+    echo ""
+    }
+
     else if (empty($cgpa)|| empty($tch) || $cgpa === 0 || $tch === 0) {
     echo "<i>This Semester:</i>"."<br>";
     echo "Total Credits Hours: ".$totalCredit."<br>";
